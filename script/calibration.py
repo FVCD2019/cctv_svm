221b5078dad1dde110b571be1eff54948bdbacd6
hyunjee RYU <ryuhj0915@gmail.com>
2019-10-11 18:35:42 +0900

    complete to porting calibration code

--- script/calibration.py
+++ script/calibration.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+import rospy
 import numpy as np
 import cv2
 import time
-
-from imutils.video import FileVideoStream
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 img_size = (1920, 1080)
 
@@ -13,37 +15,55 @@ K = np.array(
 
 D = np.array([0.01199269, -0.01029937, 0, 0, 0])
 
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(K, D, img_size, 1, img_size)
-mapx, mapy = cv2.initUndistortRectifyMap(K, D, None, newcameramtx, img_size, 5)
+class DISTORT:
+    def undistortion(self,cam_id=0):
+        print("undistort")
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(K, D, img_size, 1, img_size)
+        mapx, mapy = cv2.initUndistortRectifyMap(K, D, None, newcameramtx, img_size, 5)
+        resource = "/dev/video" + str(cam_id)
+        cap = cv2.VideoCapture(resource)
 
-fvs = FileVideoStream(1).start()
+        if (cap.isOpened()):
+            image_pub0 = rospy.Publisher("distort_cam0",Image, queue_size=1)
+            #image_pub1 = rospy.Publisher("distort_cam1",Image, queue_size=1)
+            #image_pub2 = rospy.Publisher("distort_cam2",Image, queue_size=1)
+            #image_pub3 = rospy.Publisher("distort_cam3",Image, queue_size=1)
+            bridge = CvBridge()
 
-# loop over frames from the video file stream
-while fvs.more():
-    t1 = time.time()
+            # loop over frames from the video file stream
+            while not rospy.is_shutdown():
+                t1 = time.time()
+                rval, frame = cap.read()
+                frame = cv2.resize(frame, img_size)
 
-    frame = fvs.read()
-    frame = cv2.resize(frame, img_size)
+                #cv2.imshow("original", cv2.resize(frame, (1280, 720)))
 
-    t2 = time.time()
+                # undistort
+                dst = cv2.remap(frame, mapx, mapy, cv2.INTER_LINEAR)
 
-    cv2.imshow("original", cv2.resize(frame, (1280, 720)))
+                # crop the image
+                x, y, w, h = roi
+                dst = dst[y:y + h, x:x + w]
+                t3 = time.time()
 
-    t3 = time.time()
+                print("FPS : ", 1 / (t3-t1))
 
-    # undistort
-    dst = cv2.remap(frame, mapx, mapy, cv2.INTER_LINEAR)
+                # show the frame and update the FPS counter
+                dst = cv2.resize(dst, (1280, 720))
+                image_pub0.publish(bridge.cv2_to_imgmsg(dst,"bgr8"))
+                #cv2.imshow("Frame", dst)
+                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #    break
+        else :
+            print("unable to open cam")
 
-    # crop the image
-    x, y, w, h = roi
-    dst = dst[y:y + h, x:x + w]
-    t4 = time.time()
+    def run(self):
+        print("init")
+        # Ros init
+        rospy.init_node('cam_cali', anonymous=True)
+        self.undistortion(cam_id=0)
 
-    print("FPS : ", 1 / (t4-t3+t2-t1))
 
-    # show the frame and update the FPS counter
-    dst = cv2.resize(dst, (1280, 720))
-
-    cv2.imshow("Frame", dst)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+######### MAIN ###########
+distort = DISTORT()
+distort.run()
