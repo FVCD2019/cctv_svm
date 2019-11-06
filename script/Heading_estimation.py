ed8180a3634af9af3fb7ebdfaa55457c63911d87
Beom <qjadud1994@kaist.ac.kr>
2019-11-06 19:51:20 +0900

    Update Heading_estimation.py

--- script/Heading_estimation.py
+++ script/Heading_estimation.py
@@ -4,6 +4,11 @@ import numpy as np
 from generator import vehicle_generator
 from detector import Vehicle_Detector
 
+a = np.pi/180
+
+
+
+
 """ video writer """
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 video_writer = cv2.VideoWriter('vehicle_output.avi', fourcc, 1.0, (600, 600))
@@ -39,4 +44,51 @@ for compose in range(num_images):
 
 cv2.destroyAllWindows()
 
+
+
+camera_cen_x = 651
+camera_cen_y = 814
+dis = [0,0,0,0]
+min_1 = [9999, 0]
+
+for i in range(4):
+    dis[i] = math.sqrt((camera_cen_x - rect[i,0])**2+(camera_cen_y - rect[i,1])**2)
+    if (dis[i] < min_1[0]):
+        min_1[0] = dis[i]
+        min_1[1] = i
+        
+print("rect : ", rect)
+print("dis : ", dis)
+print("min_1 : ", min_1) # rect[min_1[1]] -> the coordinate of point which is fisrtly closest point from camera center
+
+car_center = [car_center_x, car_center_y] # car center (not revised)
+revised_car_center = [0,0]
+
+print("headingangle :", headingangle)
+Rotation = np.array([[math.cos(a*headingangle), -math.sin(a*headingangle)],[math.sin(a*headingangle), math.cos(a*headingangle)]])
+print("Rotation : ", Rotation)
+ori_mat = np.array([[60, 60, -60, -60], [118, -118, 118, -118]])
+o_x = rect[min_1[1],0]
+o_y = rect[min_1[1],1]
+c_x = car_center_x
+c_y = car_center_y
+ori_point = np.array([[o_x,o_x,o_x,o_x],[o_y,o_y,o_y,o_y]])
+print("The minimum distance point is : ", [o_x, o_y])
+
+new_mat = np.matmul(Rotation,ori_mat)+ori_point
+print("The 4 candidate center point are : ", new_mat) # 1row is x point, 2row is y point
+cen_mat = np.array([[c_x,c_x,c_x,c_x],[c_y,c_y,c_y,c_y]])
+dis_mat = new_mat - cen_mat
+dis_mat = np.matmul(np.transpose(dis_mat), dis_mat)
+
+min = np.min([dis_mat[0,0],dis_mat[1,1],dis_mat[2,2],dis_mat[3,3]])
+idx = 0 # idx is number of index order about candidate 4 center point
+for i in range(4):
+    if(dis_mat[i,i] == min):
+        idx = i
+
+print("car_center : ", car_center_x, car_center_y)
+print("revised_car_center is : ", new_mat[0,idx], new_mat[1,idx])
+print("GT_car_center : ", 211, 433, "and ", 991.3, 460.1)
+print("rect : ", rect)
 print("\ndone")
