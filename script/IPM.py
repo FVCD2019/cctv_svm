80ced3bcadfe41f216f61af6c553ccbf327325e5
hcyoo93 <47550643+hcyoo93@users.noreply.github.com>
2019-11-19 01:03:09 +0900

    Add files via upload

--- script/IPM.py
+++ script/IPM.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 
 img_size = (1920, 1080)
-width, height = 1920, 1080
+width, height = 1300, 1200
 # src_pts = np.float32(
 #     [[(681, 557), (1177, 597), (1268 , 808), (503, 784)],
 #     [(668, 494), (1203, 560), (1292 , 786), (563, 733)],
@@ -28,14 +28,13 @@ class IPM:
         self.image_sub0 = rospy.Subscriber("/distort_cam0", Image, self.ipmCB0)
         self.image_sub1 = rospy.Subscriber("/distort_cam1", Image, self.ipmCB1)
 
-        self.src_pts = np.float32(
-            [[(681, 557), (1177, 597), (1268 , 808), (503, 784)],
-            [(668, 494), (1203, 560), (1292 , 786), (563, 733)],
-            [(671, 510), (1229, 493), (1301 , 721), (587, 750)],
-            [(751, 428), (1276, 498), (1377, 773), (653, 616)]]
-            )
-        self.dst_pts = np.float32([(0, 0), (width, 0), (width, height), (0, height)] )
-        self.IPM_matrix = [cv2.getPerspectiveTransform(self.src_pts[n-1], self.dst_pts) for n in range(1, 5)]
+        self.src_pts0 = np.float32([[753, 170], [747, 351], [735, 649], [730, 841], [1270, 192], [1273, 364], [1065, 656], [1061, 845]])
+	self.src_pts1 = np.float32([[1805, 965], [1673, 656], [1522, 344], [1458, 221], [805, 954], [859, 581], [1174, 288], [1158, 162]])
+        self.dst_pts = np.float32([[111, 5], [111, 315], [111, 815], [111, 1125], [1011, 5], [1011, 315], [651, 815], [651, 1125]])
+        
+	#self.IPM_matrix = [cv2.getPerspectiveTransform(self.src_pts[n-1], self.dst_pts) for n in range(1, 5)]
+	self.IPM_matrix_0, mask = cv2.findHomography(src_pts0, dst_pts)
+	self.IPM_matrix_1, mask = cv2.findHomography(src_pts1, dst_pts)
 
 
     def ipmCB0(self, data):
@@ -46,7 +45,7 @@ class IPM:
 
         ##cv2.imshow("img0", cv_image0)
         # IPM
-        img_IPM0 = cv2.warpPerspective(cv_image0, self.IPM_matrix[0], (width, height))
+        img_IPM0 = cv2.warpPerspective(cv_image0, self.IPM_matrix_0, (width, height))
         ##cv2.imshow("img_IPM0", img_IPM0)
         self.image_pub0.publish(self.bridge.cv2_to_imgmsg(img_IPM0,"bgr8"))
         # show the frame and update the FPS counter
@@ -60,7 +59,7 @@ class IPM:
             print(e)
         ##cv2.imshow("img0", cv_image1)
         # IPM
-        img_IPM1 = cv2.warpPerspective(cv_image1, self.IPM_matrix[1], (width, height))
+        img_IPM1 = cv2.warpPerspective(cv_image1, self.IPM_matrix_1, (width, height))
         ##cv2.imshow("img_IPM1", img_IPM1)
         self.image_pub1.publish(self.bridge.cv2_to_imgmsg(img_IPM1,"bgr8"))
         # show the frame and update the FPS counter
