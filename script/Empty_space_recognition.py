5cb80ab8896cce35742764a799fceafbab99e760
hyunjee RYU <ryuhj0915@gmail.com>
2019-11-19 01:18:42 +0900

    update parking_space coordinate

--- script/Empty_space_recognition.py
+++ script/Empty_space_recognition.py
@@ -24,38 +24,27 @@ class PSPACE:
         self.ps_y = 0
 
     def imageCB(self, data):
-        print("cb")
         try:
             stitch_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            print("cb1")
         except CvBridgeError as e:
             print(e)
-            print("cb2")
 
         """ pre-defined space location """
-        upper_space = [[(100 + 400 * i, 100), (500 + 400 * i, 900)] for i in range(6)]
-        lower_space = [[(100 + 400 * i, 2200), (500 + 400 * i, 3000)] for i in range(4)]
+        upper_space = [[(110,0),(294,318)], [(294,0),(470,318)], [(470,0),(651,318)], [(651,0),(830,318)], [(830,0),(1009,318)]]
+        lower_space = [[(111,816),(292,1200)], [(292,816),(472,1200)], [(472,816),(651,1200)]]
 
         pre_defined_space = upper_space + lower_space # list index is space_id
-
-        #num_images = 50
-        #max_vehicle = 4
-        # for compose in range(num_images):
-        #
-        #     print("[%03d / %03d]" % (compose, num_images), end='\r')
-        #
-        #     img_space = space_generator(max_vehicle, upper_space, lower_space)
-        #
-        #     cv2.imshow("input", cv2.resize(img_space, (600, 600)))
 
         """ empty space recognition """
         empty_space_ids = Space_Detector(stitch_image, pre_defined_space)
         self.ps_id = empty_space_ids+1
-        self.ps_x = pre_defined_space[empty_space_ids][0]
-        self.ps_y = pre_defined_space[empty_space_ids][1]
+        self.ps_u = pre_defined_space[empty_space_ids][0]
+        self.ps_b = pre_defined_space[empty_space_ids][1]
 
+	mx = (self.ps_u[0] + self.ps_b[0]) / 2
+	my = (self.ps_u[0] + self.ps_b[0]) / 2
         # this is for local ryu
-        self.pspace_info.data = [self.ps_id, self.ps_x, self.ps_y]
+        self.pspace_info.data = [self.ps_id, mx, my]
         print("pub")
         #self.pspace_pub.publish(self.pspace_info)
 
